d2de26b658905e7ae439bc6fdacbbc879b59ae24
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-20 19:09:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "1282650520:AAGWZwl7LW4WYFSPyVSpEgTEI1LyRbVLV-U"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "HellbotGit "
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/Hellboy-Op/GitGram"
