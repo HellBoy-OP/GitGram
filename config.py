09d44c7ddc5e69eccc641b8a260f210f0023edf3
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-20 19:13:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1282650520:AAGWZwl7LW4WYFSPyVSpEgTEI1LyRbVLV-U"
+BOT_TOKEN = "1411867465:AAEHGtH1x3Qm9dPCITpAg3JeFu8SKHLVU5c"
 
 # Project Name of your Project
 PROJECT_NAME = "HellbotGit "
